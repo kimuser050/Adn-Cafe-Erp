5ec87df44fb7c716bafc7c90b75aa6edecb34f23
DESKTOP-TLE9CC4\user1 <hello>
2026-03-09 17:48:20 +0900

    init

---
+++ app/src/main/java/com/kh/app/HomeController.java
@@ -0,0 +1,10 @@
+package com.kh.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("home")
+    public void home(){}
+}
