101b32d62982d4271a2b4acad47e18ae2f32566d
DESKTOP-A413FUK\user1 <wow>
2026-03-10 09:03:43 +0900

    HR 테스트

---
+++ app/src/main/java/com/kh/app/feature/hr/HrTestController.java
@@ -0,0 +1,19 @@
+package com.kh.app.feature.hr;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+@Slf4j
+@RequiredArgsConstructor
+public class HrTestController {
+
+    @GetMapping("hr")
+    public String hr() {
+        return "hr test";
+    }
+}
