0d508855d0a0d4087c41a1c1209e0990800883b8
DESKTOP-7BF8T50\user1 <wldghs5s@gmail.com>
2026-03-11 16:14:14 +0900

    feat: 계정 insert/delete/list 백엔드 기능 구현

--- app/src/main/java/com/kh/app/feature/finance/Account/AccountRestController.java
+++ app/src/main/java/com/kh/app/feature/finance/Account/AccountRestController.java
@@ -2,8 +2,12 @@ package com.kh.app.feature.finance.Account;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -12,4 +16,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountRestController {
 
     private final AccountService accountService;
+
+    @PostMapping("/insertAccount")
+    public ResponseEntity<Integer> insertAccount(@RequestBody AccountVo vo){
+        int result = accountService.insertAccount(vo);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("result" , String.valueOf(result));
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Integer> deleteAccount(@RequestParam String accountNo){
+
+        int result = accountService.deleteAccount(accountNo);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("result" , String.valueOf(result));
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/list")
+    public ResponseEntity<Map<String, Object>> accountList(@RequestParam String mainAccountNo){
+        System.out.println("mainAccountNo = " + mainAccountNo);
+        List<AccountVo> voList = accountService.accountList(mainAccountNo);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("voList" , voList);
+
+        return ResponseEntity.ok(map);
+    }
 }
