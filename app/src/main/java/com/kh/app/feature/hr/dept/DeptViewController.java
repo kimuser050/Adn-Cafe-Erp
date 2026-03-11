bf9e3fb8f66349bc69b43fdeb265029641271da1
DESKTOP-A413FUK\user1 <wow>
2026-03-11 12:01:45 +0900

    feat : dept front test create

--- app/src/main/java/com/kh/app/feature/hr/dept/DeptViewController.java
+++ app/src/main/java/com/kh/app/feature/hr/dept/DeptViewController.java
@@ -1,7 +1,15 @@
 package com.kh.app.feature.hr.dept;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("hr/dept")
 public class DeptViewController {
+
+    @GetMapping("list")
+    public String list() {
+        return "hr/dept/deptList";
+    }
 }
---
+++ app/src/main/resources/static/js/hr/dept/deptList.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("deptList 화면 확인용");
+});
