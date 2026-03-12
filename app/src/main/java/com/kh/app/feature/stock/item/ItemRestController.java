a1929600d9dc0cc7ec6788351f40d709241c38ee
DESKTOP-TLE9CC4\user1 <hello>
2026-03-12 12:06:22 +0900

    test

--- app/src/main/java/com/kh/app/feature/stock/item/ItemRestController.java
+++ app/src/main/java/com/kh/app/feature/stock/item/ItemRestController.java
@@ -10,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/item")
 @RestController
 public class ItemRestController {
+    public void test(){
+        System.out.println("test");
+    }
 }
