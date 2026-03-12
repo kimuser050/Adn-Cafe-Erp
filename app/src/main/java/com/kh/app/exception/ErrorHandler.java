2a3628b64da614967641bb966d9a33cea50c071c
DESKTOP-A413FUK\user1 <wow>
2026-03-12 10:58:36 +0900

    chore : errorHandler classes create

---
+++ app/src/main/java/com/kh/app/exception/ErrorHandler.java
@@ -0,0 +1,14 @@
+package com.kh.app.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String m01(){
+        System.out.println("Except");
+        return "redirect:/err";
+    }
+}
