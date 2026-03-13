904acf0da9816a2dbf7f36aa252daa1cafe2bd66
DESKTOP-A413FUK\user1 <wow>
2026-03-13 17:43:10 +0900

    feat : hr store create & edit (backend)

--- app/src/main/java/com/kh/app/feature/hr/store/StoreService.java
+++ app/src/main/java/com/kh/app/feature/hr/store/StoreService.java
@@ -4,14 +4,27 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Slf4j
 public class StoreService {
+
+    private final StoreMapper storeMapper;
+
+    //0. 신규부서 등록하기
+    @Transactional
+    public int insert(StoreVo vo) {
+        return storeMapper.insert(vo);
+    }
+
+    public List<StoreVo> selectList() {
+        return storeMapper.selectList();
+    }
 
 
 }
