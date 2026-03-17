1848e6fcd4d2e700ff006802f3b16c6aef1bdb3a
DESKTOP-TLE9CC4\user1 <hello>
2026-03-17 13:21:24 +0900

    feat: 입고내역 조회

--- app/src/main/java/com/kh/app/feature/stock/inbound/InboundRestController.java
+++ app/src/main/java/com/kh/app/feature/stock/inbound/InboundRestController.java
@@ -1,14 +1,47 @@
 package com.kh.app.feature.stock.inbound;
 
+import com.kh.app.feature.util.PageVo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/inbound")
 @Slf4j
 public class InboundRestController {
-    private final InboundMapper inboundMapper;
+    private final InboundService inboundService;
+
+    @Value("${page.pageLimit}")
+    private int pageLimit;
+
+    @Value("${page.boardLimit}")
+    private int boardLimit;
+
+    //입고 내역 조회만
+    @GetMapping("selectList")
+    public ResponseEntity<Map<String, Object>> selectList(@RequestParam(required = false, defaultValue = "1")int currentPage){
+        int listCount = inboundService.selectCount();
+        int pageLimit = this.pageLimit;
+        int boardLimit = this.boardLimit;
+        PageVo pvo = new PageVo(listCount, currentPage, pageLimit, boardLimit);
+        List<InboundVo> voList = inboundService.selectList(pvo);
+        Map<String, Object>map = new HashMap<>();
+        map.put("pvo",pvo);
+        map.put("voList",voList);
+        return ResponseEntity.ok(map);
+
+    }
+
+
+
 }
--- app/src/main/java/com/kh/app/feature/stock/inbound/InboundVo.java
+++ app/src/main/java/com/kh/app/feature/stock/inbound/InboundVo.java
@@ -4,4 +4,16 @@ import lombok.Data;
 
 @Data
 public class InboundVo {
+    private String deletedYn;
+    private String inDate;
+    private String quantity;
+    private String unitPrice;
+    private String itemNo;
+    private String inNo;
+    private String totalPrice;
+    private String updatedAt;
+    private String reason;
+    //JOIN
+    private String itemName;
+    private String location;
 }
