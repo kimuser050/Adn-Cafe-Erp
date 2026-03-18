a3d5799c31b3d55e58bd57f0c8b0cb9e087e71df
DESKTOP-LVN0GBM\user1 <HarryPotter>
2026-03-18 12:49:08 +0900

    feat : 공지사항 입력 css하기 직전

--- app/src/main/java/com/kh/app/feature/user/notice/NoticeMapper.java
+++ app/src/main/java/com/kh/app/feature/user/notice/NoticeMapper.java
@@ -94,7 +94,7 @@ public interface NoticeMapper {
                 )
                 VALUES
                 (
-                #{noticeNo}
+                SEQ_NOTICE.CURRVAL 
                 ,#{originName}
                 ,#{changeName}
                 ,#{filePath}
@@ -146,3 +146,4 @@ public interface NoticeMapper {
     List<NoticeFileVo> selectFileListByNoticeNo(String noticeNo);
 
 }
+
