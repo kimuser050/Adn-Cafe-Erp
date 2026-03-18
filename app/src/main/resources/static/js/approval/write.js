da32bc4fb7e2e3c38f2f06d5feafd7ca72b514d1
DESKTOP-DCKDNGE\user1 <chuhyunjong>
2026-03-18 15:07:21 +0900

    feat : 결재문서 작성 프론트엔드 초기 세팅

--- app/src/main/resources/static/js/approval/write.js
+++ app/src/main/resources/static/js/approval/write.js
@@ -1,5 +1,15 @@
 async function insertDocument(){
     try {
+        const categoryNo = document.querySelector("#categoryNo").value;
+        const writerNo = document.querySelector("#writerNo").value;
+        const deptCode = document.querySelector("#deptCode").value;
+        const title = document.querySelector("#title").value;
+        const reason = document.querySelector("#reason").value;
+        const content = document.querySelector("#content").value;
+        const approverNo = document.querySelector("#approverNo").value;
+        const startDate = document.querySelector("#startDate").value;
+        const endDate = document.querySelector("#endDate").value;
+
         const resp = await fetch(`/api/approval/document/write` , {
             method : "POST",
             headers : {
