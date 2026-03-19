20ba73638c0feeaa6b30135164409c67a33bb10d
DESKTOP-A413FUK\user1 <wow>
2026-03-19 15:09:58 +0900

    feat : att start

---
+++ app/src/main/java/com/kh/app/feature/hr/att/attStatusVo.java
@@ -0,0 +1,18 @@
+package com.kh.app.feature.hr.att;
+
+import lombok.Data;
+
+@Data
+public class attStatusVo {
+    private String attNo;
+    private String statusCode;
+    private String empNo;
+    private String workDate;
+    private String checkInAt;
+    private String checkOutAt;
+    private String attNote;
+    private String createdAt;
+    private String updatedAt;
+    private String otApprovedHours;
+    private String otConfirmedHours;
+}
